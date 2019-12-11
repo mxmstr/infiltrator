63e753c5ff5e806973702ed595372679e7753401
mxmstr <lynch.er18@gmail.com>
2019-12-11 14:17:06 -0500

    Scene loader

---
+++ Scripts/Menu.Scenes.gd
@@ -0,0 +1,30 @@
+extends Node
+
+onready var list = find_node('ListContainer')
+onready var hint = find_node('Hint')
+
+
+func _ready():
+	
+	var files = []
+	var dir = Directory.new()
+	dir.open('res://Scenes/')
+	dir.list_dir_begin()
+	
+	while true:
+		
+		var file = dir.get_next()
+		
+		if file == '':
+			break
+			
+		elif file.ends_with('.tscn'):
+			
+			var item = load('res://Scenes/UI/Menu.Scenes.Item.tscn').instance()
+			item.name = file.replace('.tres', '')
+			list.add_child(item)
+			
+			var button = item.get_node('Button')
+			button.text = file.replace('.tres', '')
+			button.connect('pressed', get_tree(), 'change_scene', ['res://Scenes/' + file])
+	
